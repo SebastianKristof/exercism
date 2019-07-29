6c84c930f657343a265a90526206dd97ff2f405a
Sebastian Kristof <cem@qlik.com>
2019-07-29 09:39:53 -0400

    final solution

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,21 +1,28 @@
+/*
+Package diffsquares teaches us that an efficient algorithm can make programs significantly faster.
+The difference of the speed of execution compared to the iterative solutions (commented out)
+is over 100-fold.
+*/
 package diffsquares
 
-func SquareOfSum(n int) (sqs int) {
-	for i := 1; i <= n; i++ {
-		sqs = sqs + i
-	}
-	sqs = sqs * sqs
-	return
+// SquareOfSum calculates the squared sum of the first n natural numbers.
+func SquareOfSum(n int) int {
+	// for i := 1; i <= n; i++ {
+	// 	sum = sum + i
+	// }
+	sum := n * (n + 1) / 2
+	return sum * sum
 }
 
-func SumOfSquares(n int) (ssq int) {
+// SumOfSquares calculates the sum of squares of the first n natural numbers.
+func SumOfSquares(n int) int {
 	// for i := 1; i <= n; i++ {
 	// 	ssq += i * i
 	// }
-	ssq = n * (n + 1) * (2*n + 1) / 6
-	return
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
+// Difference calculates the difference between the squared sum and the sum of squares of the first n natural numbers.
 func Difference(n int) int {
 	return SquareOfSum(n) - SumOfSquares(n)
 }
